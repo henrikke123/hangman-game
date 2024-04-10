3596a44c0b6f85240f1886c6f69db7a79208209e
henrikke123 <henrikkero@hotmail.no>
2024-04-10 20:32:57 +0200

    word list forbedret

--- scripts/word-list.js
+++ scripts/word-list.js
@@ -1,7 +1,62 @@
 const wordList = [
     {
-        word: "gitar",
-        hint: "A musical instrument with strings."
+        word: "sjokolade",
+        hint: "En deilig søtsak laget av kakaobønner"
     },
-    
+    {
+        word: "ekskursjon",
+        hint: "En reise eller utflukt, ofte med et pedagogisk formål"
+    },
+    {
+        word: "fantastisk",
+        hint: "Utrolig flott eller imponerende"
+    },
+    {
+        word: "forventning",
+        hint: "Følelse av forventning eller spenning"
+    },
+    {
+        word: "kommunikasjon",
+        hint: "Overføring av informasjon eller meldinger mellom individer eller enheter"
+    },
+    {
+        word: "universitet",
+        hint: "En høyere utdanningsinstitusjon som tilbyr akademiske grader"
+    },
+    {
+        word: "fornøyelsespark",
+        hint: "Et sted med attraksjoner og underholdning, vanligvis for familier"
+    },
+    {
+        word: "observatorium",
+        hint: "Et sted utstyrt for å observere himmellegemer og fenomener"
+    },
+    {
+        word: "spennende",
+        hint: "Fylt med spenning eller interesse"
+    },
+    {
+        word: "romantisk",
+        hint: "Preget av romantikk eller kjærlighet"
+    },
+    {
+        word: "situasjonskomedie",
+        hint: "En TV- eller teaterserie som fokuserer på humoristiske situasjoner i hverdagslivet"
+    },
+    {
+        word: "fotgjengerovergang",
+        hint: "En markert del av veien der fotgjengere kan krysse trygt"
+    },
+    {
+        word: "kunstgalleri",
+        hint: "En utstillingsplass for kunstverk"
+    },
+    {
+        word: "nordlysobservatorium",
+        hint: "En forskningsinstitusjon som studerer nordlys og lignende fenomener"
+    },
+    {
+        word: "veldedighetsorganisasjon",
+        hint: "En organisasjon som driver veldedighetsarbeid og hjelper mennesker i nød"
+    }
 ];
