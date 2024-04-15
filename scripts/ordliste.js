52dadc94b1e3d4ccaeb82099d32a6b6e8231e875
henrikke123 <henrikkero@hotmail.no>
2024-04-15 08:39:24 +0200

    Ny versjon

---
+++ scripts/ordliste.js
@@ -0,0 +1,62 @@
+const ordListe = [
+    {
+        word: "sjokolade",
+        hint: "En deilig søtsak laget av kakaobønner"
+    },
+    {
+        word: "ekskursjon",
+        hint: "En reise eller utflukt, ofte med et pedagogisk formål"
+    },
+    {
+        word: "ballong",
+        hint: "En luftfylt beholder laget av elastisk materiale, ofte brukt som dekorasjon"
+    },
+    {
+        word: "mysterium",
+        hint: "Noe uforklarlig"
+    },
+    {
+        word: "hestehale",
+        hint: "En frisyre"
+    },
+    {
+        word: "universitet",
+        hint: "En høyere utdanningsinstitusjon som tilbyr akademiske grader"
+    },
+    {
+        word: "magnesium",
+        hint: "Et metallisk grunnstoff som er lett og reaktivt"
+    },
+    {
+        word: "teleskop",
+        hint: "Et instrument som gir et forstørret bilde av himmelen"
+    },
+    {
+        word: "komedie",
+        hint: "En filmsjanger"
+    },
+    {
+        word: "forelsket",
+        hint: "En følelse"
+    },
+    {
+        word: "spagetti",
+        hint: "En middagsrett"
+    },
+    {
+        word: "handball",
+        hint: "En lagsport"
+    },
+    {
+        word: "blomkål",
+        hint: "En grønnsak"
+    },
+    {
+        word: "nordlys",
+        hint: "Fenomen på himmelen"
+    },
+    {
+        word: "stavanger",
+        hint: "En by i Norge"
+    }
+];
