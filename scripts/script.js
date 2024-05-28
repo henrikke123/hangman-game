57f71655c540244ad660fdb199a48e1f598934d4
henrikke123 <henrikkero@hotmail.no>
2024-05-28 21:42:24 +0200

    Update script.js
    lagring og henting av data

--- scripts/script.js
+++ scripts/script.js
@@ -9,14 +9,14 @@ const playAgainBtn = gameModal.querySelector("button");
 /* Variabler for gjeldende ord og antall feilgjettede bokstaver */
 let currentWord, correctLetters, wrongGuessCount;
 let ordListe = ordListe_local;
-const maxGuesses = 6; 
+const maxGuesses = 6;
  
 function resetGame() {
     // Tilbakestiller spillet til starttilstand
     correctLetters = [];
-    wrongGuessCount = 0; 
-    hangmanImage.src = "images/hangman-0.svg"; 
-    guessesText.innerText = wrongGuessCount + " / " + maxGuesses; 
+    wrongGuessCount = 0;
+    hangmanImage.src = "images/hangman-0.svg";
+    guessesText.innerText = wrongGuessCount + " / " + maxGuesses;
  
     // Viser bokstavene i ordet som tomme bokser
     var wordDisplayContent = "";
@@ -36,18 +36,11 @@ function resetGame() {
 }
  
  
-// Funksjon for å lagre data i localStorage
-const lagreDataILocalStorage = (data) => {
-    localStorage.setItem('lagretData', JSON.stringify(data)); /* lagrer data i localStorage som en JSON-streng */
-}
+// Kjør getRandomWord-funksjonen når siden lastes
+window.addEventListener('DOMContentLoaded', getRandomWord);
  
-// Funksjon for å hente data fra localStorage
-const henteDataFraLocalStorage = () => {
-    const lagretData = localStorage.getItem('lagretData'); /* henter data fra localStorage og konverterer JSON- streng til et javascript-objekt  */
-    return lagretData ? JSON.parse(lagretData) : null;
-}
- 
-function initialiserSpill() {
+// Funksjon for å velge et tilfeldig ord fra ordlisten, sette det som gjeldende ord og tilbakestille spillet
+function getRandomWord() {
  
     // Henter lagrede data fra localStorage, eller bruker standardordlisten hvis det ikke finnes lagrede data
     let ordListe_localStorage = localStorage.getItem("ordliste");
@@ -56,33 +49,20 @@ function initialiserSpill() {
     if (ordListe_localStorage) {
         ordListe = JSON.parse(ordListe_localStorage);
     }
+    else {
+        ordListe = ordListe_local;
+    }
  
     // Velger et tilfeldig ord fra den lagrede ordlisten og lagrer det som gjeldende ord
     let randomIndex = Math.floor(Math.random() * ordListe.length);
-    currentWord = ordListe[randomIndex].word;
- 
-    // Setter hintet for det valgte ordet ved å finne ordet i lagrede data og hente hintet
-    document.querySelector(".hint-text b").innerText = ordListe.find(function (item) {
-        return item.word === currentWord;
-    }).hint;
- 
-    resetGame(); 
-}
- 
- 
-// Kjør initialiserSpill-funksjonen når siden lastes
-window.addEventListener('DOMContentLoaded', initialiserSpill);
- 
-// Funksjon for å velge et tilfeldig ord fra ordlisten, sette det som gjeldende ord og tilbakestille spillet
-function getRandomWord() {
-    let randomIndex = Math.floor(Math.random() * ordListe.length);
-    let randomWordObject = ordListe[randomIndex]; 
+    let randomWordObject = ordListe[randomIndex];
     let word = randomWordObject.word;
     let hint = randomWordObject.hint;
+    currentWord = word;
  
-    currentWord = word; 
-    document.querySelector(".hint-text b").innerText = hint; 
-    resetGame(); 
+    // Setter hintet til å være lik dette ordet sitt hint
+    document.querySelector(".hint-text b").innerText = hint;
+    resetGame();
 }
  
 // Funksjon for å avslutte spillet og vise resultatet (seier eller tap)
@@ -112,33 +92,32 @@ function gameOver(isVictory) {
  
     let paragraphText = modalText + ' <b>' + currentWord + '</b>';
  
-    gameModal.querySelector("img").src = imageUrl; 
-    gameModal.querySelector("h4").innerText = headingText; 
-    gameModal.querySelector("p").innerHTML = paragraphText; 
-    gameModal.classList.add("show"); 
+    gameModal.querySelector("img").src = imageUrl;
+    gameModal.querySelector("h4").innerText = headingText;
+    gameModal.querySelector("p").innerHTML = paragraphText;
+    gameModal.classList.add("show");
 }
  
 function initGame(button, clickedLetter) {
     clickedLetter = clickedLetter.toLowerCase();
     // Sjekker om den gjettete bokstaven er en del av det gjeldende ordet
     if (currentWord.indexOf(clickedLetter) !== -1) {
-        console.log("AUG")
         // Går gjennom hvert tegn i det gjeldende ordet
         for (let i = 0; i < currentWord.length; i++) {
             let letter = currentWord[i];
             if (letter === clickedLetter) {
-                correctLetters.push(letter); 
+                correctLetters.push(letter);
                 let listItems = wordDisplay.querySelectorAll("li");
-                listItems[i].innerText = letter; 
-                listItems[i].classList.add("guessed"); 
+                listItems[i].innerText = letter;
+                listItems[i].classList.add("guessed");
             }
         }
     } else {
         wrongGuessCount++; // Øker antallet feilgjettede bokstaver
-        hangmanImage.src = "images/hangman-" + wrongGuessCount + ".svg"; 
+        hangmanImage.src = "images/hangman-" + wrongGuessCount + ".svg";
     }
-    button.disabled = true; 
-    guessesText.innerText = wrongGuessCount + " / " + maxGuesses; 
+    button.disabled = true;
+    guessesText.innerText = wrongGuessCount + " / " + maxGuesses;
  
     // Kaller gameOver-funksjonen hvis antallet feilgjettede bokstaver er likt maksimalt tillatt
     if (wrongGuessCount === maxGuesses) {
@@ -154,17 +133,17 @@ function initGame(button, clickedLetter) {
 // Oppretter en array som inneholder Unicode-kodepunktene for alle bokstavene, inkludert Æ, Ø og Å
 let allLetters = [];
 for (let i = 97; i <= 122; i++) {
-    allLetters.push(i); 
+    allLetters.push(i);
 }
 allLetters.push(198, 216, 197); // Legger til kodepunktene for Æ, Ø og Å
  
 // Oppretter tastene på tastaturet og legger til eventlyttere for hver tast
 for (let j = 0; j < allLetters.length; j++) {
-    let button = document.createElement("button"); 
-    button.innerText = String.fromCharCode(allLetters[j]); 
-    keyboardDiv.appendChild(button); 
+    let button = document.createElement("button");
+    button.innerText = String.fromCharCode(allLetters[j]);
+    keyboardDiv.appendChild(button);
     button.addEventListener("click", function (e) {
-        initGame(e.target, e.target.innerText); 
+        initGame(e.target, e.target.innerText);
     });
 }
  
